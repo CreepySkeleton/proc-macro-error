7e71de21c6c559931308c77bd984bef6784420f3
CreepySkeleton <creepy-skeleton@yandex.ru>
2019-11-06 19:00:39 +0300

    Remove unneeded files

--- proc-macro-error/src/multi.rs
+++
@@ -1,9 +0,0 @@
-//! Facility for stacking and emitting multiple errors.
-//!
-//! [`abort!`] macro stops a proc-macro *right away*, much like in a panic-like
-//! fashion. But sometimes you *do not* want to stop right there, for example you're
-//! processing a list of attributes and want to *emit* a separate error for every
-//! mis-built attribute.
-//!
-//! The [`emit_error!`] and [`emit_call_site_error!`] macros are just for it!
-
--- proc-macro-error/src/single.rs
+++
@@ -1,4 +0,0 @@
-//! This module contains data types and functions to be used for single-error reporting.
-//!
-//! These are supposed to be used through [`abort!`] and [`abort_call_site!`],
-//! see [crate level documentation](crate).
