d7689420ee0447479f7d224bcbe95a5397f27eeb
CreepySkeleton <creepy-skeleton@yandex.ru>
2020-11-21 06:02:53 +0000

    Adjust .stderr files

---
+++ .cargo/registry/src/github.com-1ecc6299db9ec823/trybuild-1.0.35/src/cargo.rs
@@ -0,0 +1,116 @@
+use serde::Deserialize;
+use std::path::PathBuf;
+use std::process::{Command, Output, Stdio};
+
+use crate::error::{Error, Result};
+use crate::manifest::Name;
+use crate::run::Project;
+use crate::rustflags;
+
+#[derive(Deserialize)]
+pub struct Metadata {
+    pub target_directory: PathBuf,
+    pub workspace_root: PathBuf,
+}
+
+fn raw_cargo() -> Command {
+    Command::new(option_env!("CARGO").unwrap_or("cargo"))
+}
+
+fn cargo(project: &Project) -> Command {
+    let mut cmd = raw_cargo();
+    cmd.current_dir(&project.dir);
+    cmd.env(
+        "CARGO_TARGET_DIR",
+        path!(project.target_dir / "tests" / "target"),
+    );
+    cmd.arg("--offline");
+    rustflags::set_env(&mut cmd);
+    cmd
+}
+
+pub fn build_dependencies(project: &Project) -> Result<()> {
+    let _ = cargo(project).arg("generate-lockfile").status();
+
+    let status = cargo(project)
+        .arg(if project.has_pass { "build" } else { "check" })
+        .args(target())
+        .arg("--bin")
+        .arg(&project.name)
+        .args(features(project))
+        .status()
+        .map_err(Error::Cargo)?;
+
+    if status.success() {
+        Ok(())
+    } else {
+        Err(Error::CargoFail)
+    }
+}
+
+pub fn build_test(project: &Project, name: &Name) -> Result<Output> {
+    let _ = cargo(project)
+        .arg("clean")
+        .arg("--package")
+        .arg(&project.name)
+        .arg("--color=never")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status();
+
+    cargo(project)
+        .arg(if project.has_pass { "build" } else { "check" })
+        .args(target())
+        .arg("--bin")
+        .arg(name)
+        .args(features(project))
+        .arg("--quiet")
+        .arg("--color=never")
+        .output()
+        .map_err(Error::Cargo)
+}
+
+pub fn run_test(project: &Project, name: &Name) -> Result<Output> {
+    cargo(project)
+        .arg("run")
+        .args(target())
+        .arg("--bin")
+        .arg(name)
+        .args(features(project))
+        .arg("--quiet")
+        .arg("--color=never")
+        .output()
+        .map_err(Error::Cargo)
+}
+
+pub fn metadata() -> Result<Metadata> {
+    let output = raw_cargo()
+        .arg("metadata")
+        .arg("--no-deps")
+        .arg("--format-version=1")
+        .output()
+        .map_err(Error::Cargo)?;
+
+    serde_json::from_slice(&output.stdout).map_err(|err| {
+        print!("{}", String::from_utf8_lossy(&output.stderr));
+        Error::Metadata(err)
+    })
+}
+
+fn features(project: &Project) -> Vec<String> {
+    match &project.features {
+        Some(features) => vec![
+            "--no-default-features".to_owned(),
+            "--features".to_owned(),
+            features.join(","),
+        ],
+        None => vec![],
+    }
+}
+
+fn target() -> Vec<&'static str> {
+    match crate::TARGET {
+        Some(target) => vec!["--target", target],
+        None => vec![],
+    }
+}
