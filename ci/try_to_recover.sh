8088c017c7b724d156025a3129e9eb9b94245a62
CreepySkeleton <creepy-skeleton@yandex.ru>
2020-11-21 08:44:18 +0300

    Add some logging to CI

--- ci/try_to_recover.sh
+++ ci/try_to_recover.sh
@@ -1,10 +1,18 @@
 set -e
 
 apt-get install -y git
-[[ "$(git rev-parse --abbrev-ref HEAD)" != master ]] && exit 1
+
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+echo "BRANCH: $BRANCH"
+
+[[ "$BRANCH" != master ]] && exit 1
+
+echo "Trying to adjust .stderr files..."
 
 export TRYBUILD=overwrite
 if cargo test --all ; then
+    echo "Adjustment succeeded"
+
     git config user.name "${GITLAB_USER_NAME}"
     git config user.email "${GITLAB_USER_EMAIL}"
     git add -A
@@ -12,5 +20,6 @@ if cargo test --all ; then
     git push "http://${GITLAB_USER_NAME}:${GITLAB_PERSONAL_ACCESS_TOKEN}@gitlab.com/CreepySkeleton/proc-macro-error" HEAD:master
     exit 0
 else
+    echo "Adjustment failed"
     exit 1
 fi
